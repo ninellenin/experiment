aecced79da4ede84f2899abf5d45bdc770457f87
Sazonova Yuliya <yulia-saz3@yandex.ru>
2018-04-10 13:36:52 +0700

    Implements test for utils & fixed getAbsolutePath().

--- StandaloneExecutor/src/standalone/utils/Utils.java
+++ StandaloneExecutor/src/standalone/utils/Utils.java
@@ -1,16 +1,24 @@
 package standalone.utils;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.eclipse.emf.common.util.URI;
 
 public class Utils {
+	/*
+	 * Returns absolute URI (org.eclipse.emf.common.util.URI) object for relativePath.
+	 */
 	public static URI getFileURI(String relativePath) {
 		File file = new File(relativePath);
-		return file.isFile() ? URI.createFileURI(file.getAbsolutePath()): URI.createURI(relativePath);
+		URI uri =  file.isFile() ? URI.createFileURI(file.getAbsolutePath()): URI.createURI(relativePath);
+		return uri;
 	}
 
 	public static String getAbsolutePath(String relativePath) {
-		return Utils.getFileURI(relativePath).path();
+		Path base = Paths.get(System.getProperty("user.dir"));
+		
+		return base.resolve(relativePath).toString();
 	}
 }
--- StandaloneExecutor/test/standalone/utils/UtilsTest.java
+++ StandaloneExecutor/test/standalone/utils/UtilsTest.java
@@ -2,18 +2,60 @@ package standalone.utils;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import org.eclipse.emf.common.util.URI;
 import org.junit.jupiter.api.Test;
 
 class UtilsTest {
-
-	@Test
-	void testGetFileURI() {
-		fail("Not yet implemented");
+	
+	String createFile() throws IOException {
+		String fileName = "some_file.txt";
+		//create file and write some string
+		OutputStream file = new FileOutputStream(fileName, false);
+		file.write("Hello\n".getBytes());
+		file.close();
+		
+		return fileName;
 	}
 
 	@Test
-	void testGetAbsolutePath() {
-		fail("Not yet implemented");
+	void testGetFileURIWithExistingFile() {
+		try {
+			String fileName = createFile();
+			//get URI
+			URI uri = Utils.getFileURI(fileName);
+			//check URI
+			assertTrue(uri.isFile() && !uri.isRelative());
+		} catch (IOException e) {
+			fail(e.toString());
+		}
+	}
+	@Test
+	void testGetFileURIWithAbsentFile() {
+		String absentFileName = UUID.randomUUID().toString();
+		//get URI
+		URI uri = Utils.getFileURI(absentFileName);
+		//check URI
+		assertTrue(uri.isRelative());
+	}
+
+	@Test
+	void testGetAbsolutePathWithExistingFile() {
+		try {
+			String fileName = createFile();
+			String absolutePath = Utils.getAbsolutePath(fileName);
+			Path path = Paths.get(absolutePath);
+			
+			assertTrue(path.isAbsolute());
+		} catch (IOException e) {
+			fail(e.toString());
+		}
 	}
 
 }
