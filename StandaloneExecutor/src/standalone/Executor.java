42193bcda580e5804985e775663229e5f0b62849
Sazonova Yuliya <yulia-saz3@yandex.ru>
2018-04-15 19:00:57 +0700

    Integrated Xml2Model into StandaloneExecutor.

--- StandaloneExecutor/src/standalone/Executor.java
+++ StandaloneExecutor/src/standalone/Executor.java
@@ -10,10 +10,13 @@ import org.emftext.language.presentation.scenario.scenarioPackage;
 import org.emftext.language.presentation.resource.sce.mopp.SceResourceFactory;
 
 import org.psychopy.experiment.experimentPackage;
+import org.psychopy.experiment.util.experimentResourceFactoryImpl;
+
 import standalone.emftext.DomainSpecificLanguage;
 import standalone.emftext.EmfTextExecutor;
 import standalone.epsilon.etl.EtlExecutor;
 import standalone.utils.Utils;
+import standalone.xml2model.Xml2Model;
 
 public class Executor {
 	protected static String CONFIGURATION_NOT_FOUND_MESSAGE = "Configuration file \"%s\" not found.";
@@ -21,6 +24,7 @@ public class Executor {
 	protected static String DEFAULT_CONFIGURATION = "configuration.ini";
 	protected static String EPSILON_TRANSFORMATION_FAILED_MESSAGE = "Epsilon transformation \"%s\" failed.";
 	protected static String GENERATION_TEXT_FAILED = "Text generation failed.";
+	protected static String PSYCHOPY = "PSYCHOPY";
 	protected static String PIM = "EXPERIMENT";
 	protected static String PROPERTY_NOT_FOUND_EXCEPTION = "Property \"%s\" not found.";
 	protected static String PSM = "SCENARIO";
@@ -47,6 +51,7 @@ public class Executor {
 	public static void main(String[] args) throws Exception {
 		Executor executor = new Executor(DEFAULT_CONFIGURATION);
 		
+		executor.executeXML2ModelTransformation();
 		executor.executePIM2PSMTransformation();
 		executor.executeTextGeneration();
 	}
@@ -82,6 +87,13 @@ public class Executor {
 			return properties.split(SPLIT);
 		}
 		return null;
+	}
+	
+	public void executeXML2ModelTransformation() {
+		Xml2Model psyExp2Model = new Xml2Model(new experimentResourceFactoryImpl());
+		String psyExperiment = getProperty(PSYCHOPY);
+		String[] model  = getProperties(PIM);
+		psyExp2Model.transform(psyExperiment, model[1]);
 	}
 	
 	public void executePIM2PSMTransformation() {
--- StandaloneExecutor/src/standalone/epsilon/etl/EtlExecutor.java
+++ StandaloneExecutor/src/standalone/epsilon/etl/EtlExecutor.java
@@ -1,11 +1,16 @@
 package standalone.epsilon.etl;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.epsilon.eol.IEolExecutableModule;
 import org.eclipse.epsilon.eol.models.IModel;
 import org.eclipse.epsilon.etl.EtlModule;
+import org.psychopy.experiment.util.experimentResourceFactoryImpl;
+import org.psychopy.experiment.util.experimentResourceImpl;
 
 import standalone.Model;
 import standalone.epsilon.EpsilonStandalone;
@@ -31,6 +36,13 @@ public class EtlExecutor extends EpsilonStandalone {
 	public List<IModel> getModels() throws Exception {
 		List<IModel> models = new ArrayList<IModel>();
 		models.add(createInputEmfModel(source));
+		/*//InMemoryEmfModel
+		experimentResourceFactoryImpl resourceFactory = new experimentResourceFactoryImpl();
+		Resource experimentResource = resourceFactory.createResource(Utils.getEmfURI(source.getModel()));
+		Map<Object, Object> options = new HashMap<Object, Object>();
+		experimentResource.load(options);
+		System.out.println(".....");
+		models.add(createInputInMemoryEmfModel(source, experimentResource));*/
 		models.add(createOutputEmfModel(target));
 		
 		return models;
